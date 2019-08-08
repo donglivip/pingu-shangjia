876396b498edcb42a4e4fcd2f66e62664ce05939
dongxiaoli <1076454380@qq.com>
2019-08-08 17:29:03 +0800

    bug

--- js/url.js
+++ js/url.js
@@ -1,4 +1,4 @@
 // var url_url ='http://192.168.2.107'
 // var url_url ='http://192.168.2.153'
-   // var url_url ='http://192.168.2.153'
-var url_url='http://47.105.70.236'
+   var url_url ='http://192.168.2.153'
+// var url_url='http://47.105.70.236'
